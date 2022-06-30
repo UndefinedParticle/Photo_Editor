7412ca3e131c8f9222af5e091403102628b2772f
Chinmoy Mondal <5gsanmoy@gmail.com>
2022-06-30 10:57:23 +0530

    This is a photo editing apk, Here I used Image Picker Library Dhaval and DS Photo Editor SDK

---
+++ app/src/main/java/com/example/myeditor/Splash.java
@@ -0,0 +1,36 @@
+package com.example.myeditor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        getSupportActionBar().hide();
+        Thread thread=new Thread(){
+            public void run(){
+                try {
+                    sleep(3000);
+
+                }catch (Exception e){
+                    e.printStackTrace();
+
+                }finally {
+                    Intent intent=new Intent(Splash.this,MainActivity.class);
+                    startActivity(intent);
+                    finish();
+
+                }
+            }
+
+        };thread.start();
+
+
+
+    }
+}
